9ef1ce2b4def080a3e69eb8bd8e84c5dc426156b
itay04 <itaisani121233@gmail.com>
2023-02-23 17:50:08 +0200

    addingcardproduct

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useState } from "react";
+import { Button, Card } from "react-bootstrap";
 
 
 function Product({ product }) {
@@ -14,23 +15,25 @@ function Product({ product }) {
     
   
     return (
-      <div>
-        <h1>{product.name}</h1>
-        <h3>{product.description}</h3>
-        <h3>${product.price}</h3>
-        <img
-          style={{ width: "100%" }}
-          src={"http://localhost:8000/static" + product.image}
-          alt={product.name}
-        />
-        <input
-          type="number"
-          value={quantity}
-          onChange={(e) => setQuantity(e.target.value)}
-        />
-        <button onClick={() => addToCart(product.id)}>Add To Cart</button>
-      </div>
-    )
-}
-
-export default Product
+      <Card>
+        <Card.Img variant="top" src={"http://localhost:8000/static" + product.image} alt={product.name} />
+        <Card.Body>
+          <Card.Title>{product.name}</Card.Title>
+          <Card.Text>{product.description}</Card.Text>
+          <Card.Text>${product.price}</Card.Text>
+          <div className="d-flex align-items-center">
+            <input
+              type="number"
+              value={quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+              className="form-control me-2"
+              style={{ maxWidth: "6rem" }}
+            />
+            <Button variant="primary" onClick={() => addToCart(product.id)}>Add To Cart</Button>
+          </div>
+        </Card.Body>
+      </Card>
+    );
+  }
+  
+  export default Product;
