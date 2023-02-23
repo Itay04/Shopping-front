802dae19c7a213a579a882147e0ea8061581ba3e
itay04 <itaisani121233@gmail.com>
2023-02-23 19:20:53 +0200

    editing addproduct

--- src/components/Addproduct.js
+++ src/components/Addproduct.js
@@ -4,7 +4,7 @@ function AddProduct({addProduct}) {
     const [productName, setProductName] = useState('aaa')
     const [productDescription, setProductDescription] = useState('aaa')
     const [productPrice, setProductPrice] = useState('')
-    const [productReminder, setReminder] = useState(false)
+    // const [productReminder, setReminder] = useState(false)
 
     // python
     // taskName = 'aaa'
@@ -14,31 +14,31 @@ function AddProduct({addProduct}) {
 
     function mySubmit(e) {
         e.preventDefault()
-        setProductName(productName, productDescription, productPrice, productReminder)
+        setProductName(productName, productDescription, productPrice)
         setProductDescription('')
         setProductPrice('')
-        setReminder(false)
+        // setReminder(false)
         
     }
 
     return (
         <form onSubmit={mySubmit} className="add-form">
             <div className="form-control">
-                <label>Product Name</label>
+                <label>Product Name:</label>
                 <input type="text" onChange={e => setProductName(e.target.value)} value={productName}></input>
             </div>
             <div className="form-control">
-                <label>Product description</label>
+                <label>Product description: </label>
                 <input type="text" onChange={e => setProductDescription(e.target.value)} value={productDescription}></input>
             </div>
             <div className="form-control">
-                <label>Product price</label>
+                <label>Product price: </label>
                 <input type="text" onChange={e => (e.target.value)} value={productPrice}></input>
             </div>
-            <div className="form-control form-control-check">
+            {/* <div className="form-control form-control-check">
                 <label>Product reminder</label>
                 <input type="checkbox" onChange={(e) => setReminder(e.currentTarget.checked)} checked={productReminder}></input>
-            </div>
+            </div> */}
             <div className="form-control">
                 <input className="btn btn-block" type="submit" value="Submit"></input>
             </div>
